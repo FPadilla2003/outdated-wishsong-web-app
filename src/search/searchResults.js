3924b8e64385ac0e04630550dd5da31ee35f0ce3
Felipe Padilla <padilla.f@northeastern.edu>
2023-12-15 14:59:15 -0500

    finished the search with images and lyrics

--- src/search/searchResults.js
+++ src/search/searchResults.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
 
 function SearchResults({ songResults }) {
 
+    const songImageStyle = {
+        maxWidth: '25%',
+        paddingRight: '15px'
+    }
+
     var results = songResults.map((songResult, index) => (
-        <a href={`https://genius.com${songResult.result.path}`}
-            target="_blank" rel="noreferrer noopener"
-            className='list-group-item' key={index}>
-            <img className="w-25 mr-3" src={songResult.result.song_art_image_thumbnail_url} alt='song picture' />
-            <span className=''>{songResult.result.full_title}</span>
-        </a>));
+        <Row>
+            <Col>
+                <a href={`https://genius.com${songResult.result.path}`}
+                    target="_blank" rel="noreferrer noopener"
+                    className='d-flex align-items-center flex-row list-group-item'
+                    key={index}>
+                    <img className="img-fluid pr-2"
+                        src={songResult.result.song_art_image_thumbnail_url}
+                        style={songImageStyle}
+                        alt='song picture' />
+                    <span className='w-100 h-100'>{songResult.result.full_title}</span>
+                </a>
+            </Col>
+        </Row>));
+
 
     return (
         <div className='d-flex flex-column align-items-center w-75'>
